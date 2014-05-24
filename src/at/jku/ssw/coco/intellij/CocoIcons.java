09febcb0b1bd145e36d10d077f38649b6e321269
tscheinecker <thomas.scheinecker@catalysts.cc>
2014-05-24 12:11:56 +0200

    definition of the coco file icon

---
+++ src/at/jku/ssw/coco/intellij/CocoIcons.java
@@ -0,0 +1,12 @@
+package at.jku.ssw.coco.intellij;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * @author Thomas Scheinecker <a href="mailto:tscheinecker@gmail.com">tscheinecker@gmail.com</a>
+ */
+public class CocoIcons {
+    public static final Icon FILE = IconLoader.getIcon("/at/jku/ssw/coco/intellij/icons/coco-ico.gif");
+}
