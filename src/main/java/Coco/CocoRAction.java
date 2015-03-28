d016c3d36c96ac333f8887bcbb3bcf3d91af05ac
Thomas Scheinecker <tscheinecker@gmail.com>
2015-03-29 00:39:01 +0100

    improve coco/r action
     - enable / disable according to file type
     - support multiple files at one
     - show basic error / success dialogs

--- src/main/java/Coco/CocoRAction.java
+++ src/main/java/Coco/CocoRAction.java
@@ -1,21 +1,57 @@
 package Coco;
 
-import at.jku.ssw.coco.intellij.CocoLanguage;
+import at.jku.ssw.coco.intellij.psi.CocoFile;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.util.Function;
+import com.intellij.util.containers.ContainerUtil;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by Thomas on 29/12/2014.
  */
 public class CocoRAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
-        VirtualFile file = e.getData(PlatformDataKeys.VIRTUAL_FILE);
-        String path = file.getParent().getPath();
-        String fileContent = e.getData(PlatformDataKeys.FILE_TEXT);
-        String filePath = file.getPath();
+        final Project project = getEventProject(e);
+        final List<CocoFile> bnfFiles = getFiles(e);
+        if (project == null || bnfFiles.isEmpty()) return;
+        PsiDocumentManager.getInstance(project).commitAllDocuments();
+        FileDocumentManager.getInstance().saveAllDocuments();
+
+
+        for (CocoFile file : bnfFiles) {
+            generate(file);
+        }
+    }
+
+    private static List<CocoFile> getFiles(AnActionEvent e) {
+        Project project = getEventProject(e);
+        VirtualFile[] files = e.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY);
+        if (project == null || files == null) return Collections.emptyList();
+        final PsiManager manager = PsiManager.getInstance(project);
+        return ContainerUtil.mapNotNull(files, new Function<VirtualFile, CocoFile>() {
+            @Override
+            public CocoFile fun(VirtualFile file) {
+                PsiFile psiFile = manager.findFile(file);
+                return psiFile instanceof CocoFile ? (CocoFile) psiFile : null;
+            }
+        });
+    }
+
+    private void generate(CocoFile file) {
+        // TODO use correct output directory
+        String path = file.getVirtualFile().getParent().getPath();
+        String filePath = file.getVirtualFile().getPath();
 
         String nsName = null, frameDir = null, outDir = null, ddtString = null;
 
@@ -37,16 +73,21 @@ public class CocoRAction extends AnAction {
         parser.Parse();
 
         parser.trace.Close();
+        // todo show error / success to user
         System.out.println(parser.errors.count + " errors detected");
-        if (parser.errors.count != 0) {
-            throw new FatalError("failure");
+
+        if (parser.errors.count > 0) {
+            Messages.showMessageDialog(file.getProject(), parser.errors.count + " errors occured", "Error", Messages.getErrorIcon());
+        } else {
+            // todo sync virtual file system
+            Messages.showMessageDialog(file.getProject(), "Scanner and Parser successfuly generated", "Success", Messages.getInformationIcon());
         }
+
     }
 
     @Override
     public void update(AnActionEvent e) {
-        PsiFile data = e.getData(PlatformDataKeys.PSI_FILE);
-        if (data != null && data.getLanguage() == CocoLanguage.INSTANCE) {
-        }
+        List<CocoFile> files = getFiles(e);
+        e.getPresentation().setEnabledAndVisible(!files.isEmpty());
     }
 }
