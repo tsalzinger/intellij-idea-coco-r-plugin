a0a1d0e2ba34b1b39abd5c219d7d5a2700dd435a
Thomas Scheinecker <thomas@scheinecker.io>
2018-01-12 18:31:41 +0100

    remove custom completion contribution as it duplicates default behavior

--- src/main/java/at/scheinecker/intellij/coco/CocoCompletionContributor.kt
+++ src/main/java/at/scheinecker/intellij/coco/CocoCompletionContributor.kt
@@ -1,40 +1,35 @@
 package at.scheinecker.intellij.coco
 
-import at.scheinecker.intellij.coco.psi.CocoTypes
-import com.intellij.codeInsight.completion.*
-import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.patterns.PlatformPatterns
-import com.intellij.util.ProcessingContext
-import java.util.*
+import com.intellij.codeInsight.completion.CompletionContributor
 
 /**
  * Created by Thomas on 27/12/2014.
  */
 class CocoCompletionContributor : CompletionContributor() {
     init {
-        extend(CompletionType.BASIC,
-                PlatformPatterns
-                        .psiElement(CocoTypes.IDENT)
-                        .withParent(PlatformPatterns.psiElement(CocoTypes.END))
-                        .withLanguage(CocoLanguage.INSTANCE),
-                object : CompletionProvider<CompletionParameters>() {
-                    override fun addCompletions(completionParameters: CompletionParameters, processingContext: ProcessingContext?, completionResultSet: CompletionResultSet) {
-                        val compilers = CocoUtil.findCompilers(completionParameters.originalFile)
-
-                        for (compiler in compilers) {
-                            if (Objects.isNull(compiler.name)) {
-                                continue
-                            }
-
-                            val compilerNameLookupElement = LookupElementBuilder
-                                    .create(compiler.name!!)
-                                    .withIcon(CocoIcons.FILE)
-                                    .withTypeText(compiler.containingFile.name)
-                            completionResultSet.addElement(compilerNameLookupElement)
-                        }
-
-                    }
-                })
+//        extend(CompletionType.BASIC,
+//                PlatformPatterns
+//                        .psiElement(CocoTypes.IDENT)
+//                        .withParent(PlatformPatterns.psiElement(CocoTypes.END))
+//                        .withLanguage(CocoLanguage.INSTANCE),
+//                object : CompletionProvider<CompletionParameters>() {
+//                    override fun addCompletions(completionParameters: CompletionParameters, processingContext: ProcessingContext?, completionResultSet: CompletionResultSet) {
+//                        val compilers = CocoUtil.findCompilers(completionParameters.originalFile)
+//
+//                        for (compiler in compilers) {
+//                            if (Objects.isNull(compiler.name)) {
+//                                continue
+//                            }
+//
+//                            val compilerNameLookupElement = LookupElementBuilder
+//                                    .create(compiler.name!!)
+//                                    .withIcon(CocoIcons.FILE)
+//                                    .withTypeText(compiler.containingFile.name)
+//                            completionResultSet.addElement(compilerNameLookupElement)
+//                        }
+//
+//                    }
+//                })
 
 //        extend(CompletionType.BASIC,
 //                PlatformPatterns
