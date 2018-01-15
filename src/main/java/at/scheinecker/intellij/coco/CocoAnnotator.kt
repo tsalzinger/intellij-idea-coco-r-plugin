8d1fcebf3c4b1b56dfb98c377f14e7a79fbfb9c6
Thomas Scheinecker <thomas@scheinecker.io>
2018-01-15 14:59:17 +0100

    correctly handle formal output parameter in production usage

--- src/main/java/at/scheinecker/intellij/coco/CocoAnnotator.kt
+++ src/main/java/at/scheinecker/intellij/coco/CocoAnnotator.kt
@@ -10,118 +10,127 @@ import com.intellij.psi.search.searches.ReferencesSearch
 
 class CocoAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-        if (element is CocoEnd) {
-            val ident = element.nameIdentifier ?: return
+        when (element) {
+            is CocoEnd -> {
+                val ident = element.nameIdentifier ?: return
 
-            val compilerName = ident.text
-            val compiler = CocoUtil.findCompiler(element.getContainingFile(), compilerName)
+                val compilerName = ident.text
+                val compiler = CocoUtil.findCompiler(element.getContainingFile(), compilerName)
 
-            if (compiler != null) {
-                val annotation = holder.createInfoAnnotation(ident, null)
-                annotation.setTextAttributes(DefaultLanguageHighlighterColors.CLASS_REFERENCE)
-            } else {
-                holder.createErrorAnnotation(ident, "Unresolved COMPILER '$compilerName'")
-            }
-        } else if (element is HasCocoCharacterReference) {
-            val ident = element.nameIdentifier
-            if (ident != null) {
-                val characterReferenceName = element.name
-                val characterDeclaration = CocoUtil.findCharacterDeclaration(element.getContainingFile(), characterReferenceName)
-
-                if (characterDeclaration != null) {
-                    val characterOffset = characterDeclaration.textRange.startOffset
-                    val referenceOffset = element.textRange.startOffset
-
-                    if (referenceOffset < characterOffset) {
-                        holder.createErrorAnnotation(ident, "Character '$characterReferenceName' used before its defined")
-                    } else {
-                        val annotation = holder.createInfoAnnotation(ident, null)
-                        annotation.setTextAttributes(DefaultLanguageHighlighterColors.INSTANCE_FIELD)
-                    }
-                } else {
-                    holder.createErrorAnnotation(ident, "Unresolved Character '$characterReferenceName'")
-                }
-            }
-        } else if (element is CocoFactor) {
-            val ident = element.nameIdentifier
-            if (ident != null) {
-                val referenceName = element.name
-                val tokenDecl = CocoUtil.findTokenDecl(element.getContainingFile(), referenceName)
-                val production = CocoUtil.findProduction(element.getContainingFile(), referenceName)
-
-                if (tokenDecl != null) {
-                    val characterOffset = tokenDecl.textRange.startOffset
-                    val referenceOffset = element.textRange.startOffset
-
-                    if (referenceOffset < characterOffset) {
-                        holder.createWarningAnnotation(ident, "Token '$referenceName' used before its defined")
-                    } else {
-                        val annotation = holder.createInfoAnnotation(ident, null)
-                        annotation.setTextAttributes(DefaultLanguageHighlighterColors.INSTANCE_FIELD)
-                    }
-                } else if (production != null) {
+                if (compiler != null) {
                     val annotation = holder.createInfoAnnotation(ident, null)
-                    annotation.textAttributes = DefaultLanguageHighlighterColors.INSTANCE_FIELD
-
-                    if (production.formalAttributes != null && element.actualAttributes == null) {
-                        holder.createErrorAnnotation(ident, "Production '$referenceName' defines formal attributes")
-                    } else if (production.formalAttributes == null && element.actualAttributes != null) {
-                        holder.createErrorAnnotation(element.actualAttributes!!, "Production '$referenceName' doesn't define formal attributes")
-                    }
+                    annotation.setTextAttributes(DefaultLanguageHighlighterColors.CLASS_REFERENCE)
                 } else {
-                    holder.createErrorAnnotation(ident, "Unresolved Token or Production '$referenceName'")
+                    holder.createErrorAnnotation(ident, "Unresolved COMPILER '$compilerName'")
                 }
             }
-        } else if (element is CocoCompiler) {
-            val ident = element.nameIdentifier
+            is HasCocoCharacterReference -> {
+                val ident = element.nameIdentifier
+                if (ident != null) {
+                    val characterReferenceName = element.name
+                    val characterDeclaration = CocoUtil.findCharacterDeclaration(element.getContainingFile(), characterReferenceName)
 
-            if (ident != null) {
-                val annotation = holder.createInfoAnnotation(ident, null)
-                annotation.textAttributes = DefaultLanguageHighlighterColors.CLASS_NAME
-                val production = CocoUtil.findProduction(element.getContainingFile(), ident.text)
+                    if (characterDeclaration != null) {
+                        val characterOffset = characterDeclaration.textRange.startOffset
+                        val referenceOffset = element.textRange.startOffset
 
-                if (production == null) {
-                    holder.createErrorAnnotation(ident, "Missing production for '" + ident.text + "'")
-                }
-            }
-
-        } else if (element is PsiNameIdentifierOwner) {
-            val ident = element.nameIdentifier
-
-            if (ident != null) {
-                val first = ReferencesSearch.search(element).findFirst()
-
-                if (first == null && element is CocoProduction) {
-                    val compiler = CocoUtil.findCompiler(element.getContainingFile(), element.name)
-                    if (compiler != null) {
-                        val annotation = holder.createInfoAnnotation(ident, "Entry point production for grammar")
-                        annotation.textAttributes = DefaultLanguageHighlighterColors.INSTANCE_FIELD
-                        return
-                    }
-                }
-
-                if (first == null) {
-                    val warningText: String
-
-                    if (element is CocoSetDecl) {
-                        warningText = "Character '" + ident.text + "' is never used"
-                    } else if (element is CocoTokenDecl) {
-                        if (element.getParent() is CocoPragmaDecl) {
-                            warningText = "Pragma '" + ident.text + "' is never used"
+                        if (referenceOffset < characterOffset) {
+                            holder.createErrorAnnotation(ident, "Character '$characterReferenceName' used before its defined")
                         } else {
-                            warningText = "Token '" + ident.text + "' is never used"
+                            val annotation = holder.createInfoAnnotation(ident, null)
+                            annotation.setTextAttributes(DefaultLanguageHighlighterColors.INSTANCE_FIELD)
                         }
-                    } else if (element is CocoProduction) {
-                        warningText = "Production '" + ident.text + "' is never used"
                     } else {
-                        warningText = "'" + ident.text + "' is never used"
+                        holder.createErrorAnnotation(ident, "Unresolved Character '$characterReferenceName'")
+                    }
+                }
+            }
+            is CocoFactor -> {
+                val ident = element.nameIdentifier
+                if (ident != null) {
+                    val referenceName = element.name
+                    val tokenDecl = CocoUtil.findTokenDecl(element.getContainingFile(), referenceName)
+                    val production = CocoUtil.findProduction(element.getContainingFile(), referenceName)
+
+                    if (tokenDecl != null) {
+                        val characterOffset = tokenDecl.textRange.startOffset
+                        val referenceOffset = element.textRange.startOffset
+
+                        if (referenceOffset < characterOffset) {
+                            holder.createWarningAnnotation(ident, "Token '$referenceName' used before its defined")
+                        } else {
+                            val annotation = holder.createInfoAnnotation(ident, null)
+                            annotation.setTextAttributes(DefaultLanguageHighlighterColors.INSTANCE_FIELD)
+                        }
+                    } else if (production != null) {
+                        val annotation = holder.createInfoAnnotation(ident, null)
+                        annotation.textAttributes = DefaultLanguageHighlighterColors.INSTANCE_FIELD
+
+                        val formalAttributes = production.formalAttributes
+                        val hasFormalInputAttribues = formalAttributes != null && formalAttributes.text.trim('<', '>').trim().split(",").filter { !it.startsWith("out ") }.isNotEmpty()
+
+                        if (hasFormalInputAttribues && element.actualAttributes == null) {
+                            holder.createErrorAnnotation(ident, "Production '$referenceName' defines formal attributes")
+                        } else if (!hasFormalInputAttribues && element.actualAttributes != null) {
+                            holder.createErrorAnnotation(element.actualAttributes!!, "Production '$referenceName' doesn't define formal attributes")
+                        }
+                    } else {
+                        holder.createErrorAnnotation(ident, "Unresolved Token or Production '$referenceName'")
+                    }
+                }
+            }
+            is CocoCompiler -> {
+                val ident = element.nameIdentifier
+
+                if (ident != null) {
+                    val annotation = holder.createInfoAnnotation(ident, null)
+                    annotation.textAttributes = DefaultLanguageHighlighterColors.CLASS_NAME
+                    val production = CocoUtil.findProduction(element.getContainingFile(), ident.text)
+
+                    if (production == null) {
+                        holder.createErrorAnnotation(ident, "Missing production for '" + ident.text + "'")
+                    }
+                }
+
+            }
+            is PsiNameIdentifierOwner -> {
+                val ident = element.nameIdentifier
+
+                if (ident != null) {
+                    val first = ReferencesSearch.search(element).findFirst()
+
+                    if (first == null && element is CocoProduction) {
+                        val compiler = CocoUtil.findCompiler(element.getContainingFile(), element.name)
+                        if (compiler != null) {
+                            val annotation = holder.createInfoAnnotation(ident, "Entry point production for grammar")
+                            annotation.textAttributes = DefaultLanguageHighlighterColors.INSTANCE_FIELD
+                            return
+                        }
                     }
 
-                    val annotation = holder.createWeakWarningAnnotation(ident, warningText)
-                    annotation.textAttributes = DefaultLanguageHighlighterColors.LINE_COMMENT
-                } else {
-                    val annotation = holder.createInfoAnnotation(ident, null)
-                    annotation.textAttributes = DefaultLanguageHighlighterColors.INSTANCE_FIELD
+                    if (first == null) {
+                        val warningText: String
+
+                        if (element is CocoSetDecl) {
+                            warningText = "Character '" + ident.text + "' is never used"
+                        } else if (element is CocoTokenDecl) {
+                            if (element.getParent() is CocoPragmaDecl) {
+                                warningText = "Pragma '" + ident.text + "' is never used"
+                            } else {
+                                warningText = "Token '" + ident.text + "' is never used"
+                            }
+                        } else if (element is CocoProduction) {
+                            warningText = "Production '" + ident.text + "' is never used"
+                        } else {
+                            warningText = "'" + ident.text + "' is never used"
+                        }
+
+                        val annotation = holder.createWeakWarningAnnotation(ident, warningText)
+                        annotation.textAttributes = DefaultLanguageHighlighterColors.LINE_COMMENT
+                    } else {
+                        val annotation = holder.createInfoAnnotation(ident, null)
+                        annotation.textAttributes = DefaultLanguageHighlighterColors.INSTANCE_FIELD
+                    }
                 }
             }
         }
