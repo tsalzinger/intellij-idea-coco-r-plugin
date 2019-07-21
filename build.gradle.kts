f8752e8e73292b929853b503982cc6cbb26987b4
Thomas Salzinger <thomas@salzinger.me>
2019-07-21 12:50:20 +0200

    Start development for v1.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "io.scheinecker.intellij"
-version = "1.3.0"
+version = "1.3.1-SNAPSHOT"
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
