7fdd098781f847a7c109351096d31a0950750eee
Thomas Salzinger <ths@tractive.com>
2020-02-07 18:36:10 +0100

    Release v1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "me.salzinger.intellij"
-version = "1.4.0-SNAPSHOT"
+version = "1.4.0"
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
